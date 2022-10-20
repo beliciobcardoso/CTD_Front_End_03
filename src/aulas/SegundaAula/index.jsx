c90474b059d2dbf15aaacc5fa0458c8020ab5d4a
Belicio Batista Cardoso <belloinfo@gmail.com>
2022-10-20 08:28:47 -0300

    Segunda Aula concluida

---
+++ src/aulas/SegundaAula/index.jsx
@@ -0,0 +1,16 @@
+import './style.scss';
+export function SegundaAula() {
+  return (
+    <main>
+      <h1>Segunda Aula</h1>
+      <h2>Projeto escolhido: Pagina do YouTube </h2>
+      <ul>
+        <li>Header</li>
+        <li>Botão de filtro</li>
+        <li>Barra de Pesquisa</li>
+        <li>Menu Hamburg</li>
+        <li>Menu Lateral</li>
+      </ul>
+    </main>
+  );
+}
---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,3 @@
+export function Login() {
+  return <h1>Tela de Login</h1>;
+}
