877b49b755a475cf9445bca8c60189841cf53343
Belicio Batista Cardoso <belloinfo@gmail.com>
2022-11-19 22:01:03 -0300

    Decima quarta aula

---
+++ src/lessons/DecimaQuartaAulaItem/index.jsx
@@ -0,0 +1,11 @@
+import { useEffect } from 'react';
+import './style.scss';
+export function DecimaQuartaAulaItem() {
+  useEffect(() => {}, []);
+
+  return (
+    <main className='mainDecimaQuartaAulaItem'>
+      <h1>DecimaQuartaAulaItem</h1>
+    </main>
+  );
+}
